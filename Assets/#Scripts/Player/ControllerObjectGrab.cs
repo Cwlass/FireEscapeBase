17741614cc521c9c4b11ef1b996dc8e4a6fdfc7d
David <Cwlass@gmail.com>
2019-04-24 15:28:47 +0200

    Project Base
    Initial commit of whole project

---
+++ Assets/#Scripts/Player/ControllerObjectGrab.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerObjectGrab : MonoBehaviour
+{
+    private SteamVR_TrackedObject trackedObj;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+    
+    private GameObject collidingObject;
+   
+    private GameObject objectInHand;
+
+    private GameObject door;
+
+
+
+    private bool openingDoor = false;
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    private void SetCollidingObject(Collider col)
+    {
+        if (collidingObject || !col.GetComponent<Rigidbody>())
+        {
+            return;
+        }
+      
+        collidingObject = col.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Controller.GetHairTriggerDown())
+        {
+            if (collidingObject)
+            {
+                GrabObject();
+            }
+            if (openingDoor)
+                FindHinge();
+        }
+
+       
+        if (Controller.GetHairTriggerUp())
+        {
+            if (objectInHand)
+            {
+                ReleaseObject();
+            }
+            if (openingDoor)
+                ReleaseDoor();
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("PickUp"))
+            SetCollidingObject(other);
+        if (other.CompareTag("Door"))
+        {
+            openingDoor = true;
+            door = other.gameObject;
+        }
+            
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("PickUp"))
+            SetCollidingObject(other);
+        if (other.CompareTag("Door"))
+        {
+            openingDoor = true;
+            door = other.gameObject;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        openingDoor = false;
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (!collidingObject)
+        {
+            return;
+        }
+        if (other.CompareTag("Door"))
+        {
+            openingDoor = false;
+
+        }
+        collidingObject = null;
+    }
+
+    private void GrabObject()
+    {
+        
+        objectInHand = collidingObject;
+        collidingObject = null;
+        
+        var joint = AddFixedJoint();
+        joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+    }
+
+    
+    private FixedJoint AddFixedJoint()
+    {
+        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+        fx.breakForce = 20000;
+        fx.breakTorque = 20000;
+        return fx;
+    }
+
+    private void ReleaseObject()
+    {
+
+        if (GetComponent<FixedJoint>())
+        {
+
+            GetComponent<FixedJoint>().connectedBody = null;
+            Destroy(GetComponent<FixedJoint>());
+
+            objectInHand.GetComponent<Rigidbody>().velocity = Controller.velocity;
+            objectInHand.GetComponent<Rigidbody>().angularVelocity = Controller.angularVelocity;
+        }
+        objectInHand = null;
+    }
+    private void FindHinge()
+    {
+        door = door.transform.parent.transform.parent.gameObject;
+        door.GetComponent<DoorRotator>().target = this.transform;
+        door.GetComponent<DoorRotator>().Openingdoor = true; 
+    }
+
+    private void ReleaseDoor()
+    {
+        door.GetComponent<DoorRotator>().Openingdoor = false;
+        door = null;
+    }
+}
---
+++ Assets/#Scripts/Player/WalkBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkBehaviour : MonoBehaviour {
+	public GameObject gun;
+	public GameObject fire;
+	public GameObject fire2;
+
+	public float jumpLenght = 0.6f;
+	// Use this for initialization
+	void Start () {
+
+		gun = this.transform.Find("Main Camera").gameObject;
+        if (fire != null)
+        {
+            fire.SetActive(false);
+        }
+        if (fire2 != null)
+        {
+            fire2.SetActive(false);
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("f")) {// activate the fire
+            if (fire != null)
+            {
+                fire.SetActive(true);
+            }
+            if (fire2 != null)
+            {
+                fire2.SetActive(true);
+            }
+		}
+
+		if (Input.GetButtonDown("Jump")){
+			Vector3 r = gun.transform.eulerAngles;
+			Vector3 r2 = this.transform.eulerAngles;
+			r.y = r.y - r2.y;
+
+			this.transform.Translate(jumpLenght*Mathf.Sin(r.y/180.0f*Mathf.PI),0.0f,jumpLenght*Mathf.Cos(r.y/180.0f*Mathf.PI));
+
+            //Debug.Log("Jumped with r = " + r);
+        }
+
+    }
+}
